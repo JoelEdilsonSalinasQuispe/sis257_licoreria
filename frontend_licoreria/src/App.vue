e161f9f56b19c089b1bf945778dcc4ad4afce8cb
Vanessa Villarroel Choque <vane.4villarroel@gmail.com>
2025-10-23 23:56:12 -0400

    Agregar frontend para licorería

---
+++ frontend_licoreria/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import '@/assets/js/scripts.js'
+import MainHeader from '@/components/MainHeader.vue'
+import MainFooter from '@/components/MainFooter.vue'
+</script>
+
+<template>
+  <main>
+    <MainHeader />
+    <RouterView />
+  </main>
+  <MainFooter />
+</template>
+
+<style>
+@import '@/assets/css/styles.css';
+</style>
